4cc00d7da36cd5941fb2357596f8f4bdc6c6cc96
Melwin <melwinoro@yahoo.com>
2015-04-02 18:02:58 +0800

    Include MahApps

--- OmicronCreditSolutions/LoginWindow.xaml.cs
+++ OmicronCreditSolutions/LoginWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,9 +19,9 @@ namespace OmicronCreditSolutions
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginWindow : MetroWindow
     {
-        public MainWindow()
+        public LoginWindow()
         {
             InitializeComponent();
         }
